64014fde391dd48c77a2faf060ba3af2aed57ea4
Ron Cordell <ron.cordell@berkeley.edu>
2016-04-27 23:24:22 -0700

    HW13 update

--- week13/hw13/bootstrap.sh
+++ week13/hw13/bootstrap.sh
@@ -1,32 +1,62 @@
 #!/bin/bash
 set -x -e
 
-# upgrade packages
-sudo yum upgrade -y
-sudo pip install --upgrade pip
+# Determine if we are running on the master node.
+# 0 - running on master
+# 1 - running on a task or core node
+check_if_master() {
+    python - <<'__SCRIPT__'
+import sys
+import json
+ 
+instance_file = "/mnt/var/lib/info/instance.json"
+is_master = False
+try:
+    with open(instance_file) as f:
+        props = json.load(f)
+ 
+    is_master = props.get('isMaster', False)
+    print props
+except IOError as ex:
+    pass # file will not exist when testing on a non-emr machine
+ 
+if is_master:
+    sys.exit(0)
+else:
+    sys.exit(1)
+__SCRIPT__
+}
 
-#Installing Jupyter Notebook
-cd /home/hadoop
+check_if_master
+if [ 1 -eq 0 ]
+then
+	# upgrade packages
+	sudo yum upgrade -y
+	sudo pip install --upgrade pip
 
-mkdir jupyter
-cd jupyter
-virtualenv venv
-source venv/bin/activate
+	#Installing Jupyter Notebook
+	cd /home/hadoop
 
-#Install ipython and dependency
-pip install "jupyter"
-pip install requests numpy
-pip install matplotlib
-#pip install scipy
-#pip install scikit-learn
+	mkdir jupyter
+	cd jupyter
+	virtualenv venv
+	source venv/bin/activate
 
-#Create profile 	
-jupyter notebook --generate-config
+	#Install ipython and dependency
+	pip install "jupyter"
+	pip install matplotlib
 
-#Run on master /slave based on configuration
+	#Create profile 	
+	jupyter notebook --generate-config
 
-echo "c = get_config()" >  /home/hadoop/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'" >>  /home/hadoop/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.open_browser = False"  >>  /home/hadoop/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.port = 8192" >>  /home/hadoop/.jupyter/jupyter_notebook_config.py
-nohup jupyter notebook --no-browser > /mnt/var/log/jupyter_notebook.log &
+	#Run on master /slave based on configuration
+
+	echo "c = get_config()" >  /home/hadoop/.jupyter/jupyter_notebook_config.py
+	echo "c.NotebookApp.ip = '*'" >>  /home/hadoop/.jupyter/jupyter_notebook_config.py
+	echo "c.NotebookApp.open_browser = False"  >>  /home/hadoop/.jupyter/jupyter_notebook_config.py
+	echo "c.NotebookApp.port = 8192" >>  /home/hadoop/.jupyter/jupyter_notebook_config.py
+	nohup jupyter notebook --no-browser > /mnt/var/log/jupyter_notebook.log &
+else
+	echo "Not a master node, skipping"
+fi
+exit 0
--- week13/hw13/create-aws-cluster.sh
+++ week13/hw13/create-aws-cluster.sh
@@ -2,7 +2,6 @@
 aws emr create-cluster --release-label emr-4.6.0 --use-default-roles \
 --name HW13Cluster --applications Name=SPARK --log-uri s3://hadoopspark/logs/ \
 --ec2-attributes KeyName=AWS_EC2_VAGRANT \
---bootstrap-action Path=s3://elasticmapreduce/bootstrap-actions/run-if,Args=["instance.isMaster=true","s3://hadoopspark/bootstrap/bootstrap.sh"] \
 --instance-groups InstanceGroupType=MASTER,InstanceType=m3.xlarge,InstanceCount=1,BidPrice=0.04 \
 InstanceGroupType=CORE,BidPrice=0.04,InstanceType=m3.xlarge,InstanceCount=2 > cluster_ids.txt
 
